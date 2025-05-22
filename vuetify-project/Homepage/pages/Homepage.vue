10ead7df9d924aa8cf2e076222f6701932d8aa64
Azfar Azhar <ahmadazfarazhar@gmail.com>
2025-05-22 13:18:05 +0800

    Homepage v1
    Version 1 of the MIKAN homepage

---
+++ vuetify-project/Homepage/pages/Homepage.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <Sidebar />
+    <Navbar />
+    <v-main>
+      <!-- Your main content here -->
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Sidebar from "../components/Sidebar.vue";
+import Navbar from "../components/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Sidebar,
+    Navbar,
+  },
+};
+</script>
