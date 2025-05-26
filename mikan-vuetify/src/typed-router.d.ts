23c709ab9a9076a1e0cf30dfdcb8f63cdc586e3a
Avgnoodl <avgnoodlihateautonicks@gmail.com>
2025-05-26 15:16:18 +0800

    MyTasks Test

--- mikan-vuetify/src/typed-router.d.ts
+++ mikan-vuetify/src/typed-router.d.ts
@@ -20,5 +20,6 @@ declare module 'vue-router/auto-routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/Homepage': RouteRecordInfo<'/Homepage', '/Homepage', Record<never, never>, Record<never, never>>,
+    '/MyTasks': RouteRecordInfo<'/MyTasks', '/MyTasks', Record<never, never>, Record<never, never>>,
   }
 }
