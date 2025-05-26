7b2fda8fd2e43a6cb1a4103e787c82c1f88f7637
Azfar Azhar <ahmadazfarazhar@gmail.com>
2025-05-26 17:18:56 +0800

    Added Audio Converter feature

--- mikan-vuetify/src/pages/Homepage.vue
+++ mikan-vuetify/src/pages/Homepage.vue
@@ -1,22 +1,11 @@
 <template>
-  <v-app>
-    <Sidebar />
-    <Navbar />
-    <v-main>
-      <!-- Your main content here -->
-    </v-main>
-  </v-app>
+  <Sidebar />
+  <Navbar />
+  <AudioConverter />
 </template>
 
 <script>
 import Sidebar from "../components/Sidebar.vue";
 import Navbar from "../components/Navbar.vue";
-
-export default {
-  name: "App",
-  components: {
-    Sidebar,
-    Navbar,
-  },
-};
+import AudioConverter from "../components/AudioConverter.vue";
 </script>
