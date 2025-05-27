f6500d6b929b55f628594258a7199e937527660b
Jet <nyeewenjet@gmail.com>
2025-05-27 09:44:28 +0800

    add file

---
+++ mikan-vuetify/src/components/Buttons/KanbanButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-tooltip location="left">
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn
+        v-bind="activatorProps"
+        fab
+        size="x-large"
+        color="primary"
+        dark
+        class="action-button-style"
+        @click="handleClick"
+      >
+        <v-icon>mdi-view-dashboard-outline</v-icon>
+      </v-btn>
+    </template>
+    <span>View on Kanban Board</span>
+  </v-tooltip>
+</template>
+
+<script setup>
+const emit = defineEmits(['action']);
+const handleClick = () => {
+  console.log('ViewKanbanButton clicked');
+  emit('action', { type: 'viewKanban' });
+};
+</script>
+
+<style scoped>
+.action-button-style {
+  box-shadow: 0px 3px 5px -1px rgba(0,0,0,0.2), 
+              0px 6px 10px 0px rgba(0,0,0,0.14), 
+              0px 1px 18px 0px rgba(0,0,0,0.12);
+}
+</style>
