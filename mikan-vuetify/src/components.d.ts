faaf7c3896ce1960e543520593fc2adac1c19c5c
Kang-1011 <148540080+Kang-1011@users.noreply.github.com>
2025-05-27 10:15:28 +0800

    Revert "Add Minutes Component, Minutes Page, Transcript Page"
    This reverts commit 2aaa795c93b217522e27d4058676c5fcbaaa094c.

--- mikan-vuetify/src/components.d.ts
+++ mikan-vuetify/src/components.d.ts
@@ -10,7 +10,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     AppFooter: typeof import('./components/AppFooter.vue')['default']
     HelloWorld: typeof import('./components/HelloWorld.vue')['default']
-    MinuteDisplay: typeof import('./components/MinuteDisplay.vue')['default']
     Navbar: typeof import('./components/Navbar.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
