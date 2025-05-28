5ef560be9755622ed32b0c21c848499ecd5831fd
RohimiRaslam <107778941+RohimiRaslam@users.noreply.github.com>
2025-05-28 14:21:11 +0800

    First commit delete confirmation popup (#12)

---
+++ mikan-vuetify/src/components/PopUpConfirmDelete.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="4">
+      <v-card align="center" class="bg-white pa-2" rounded="xl">
+        <v-icon color="red" size="150" border>mdi-close</v-icon>
+        <v-card-title>Delete task {{ task }}?</v-card-title>
+        <v-card-text>Deleted task will be gone forever</v-card-text>
+        <v-row justify="center">
+          <v-col cols="auto">
+            <v-btn color="red" size="x-large" rounded="lg">Delete</v-btn>
+          </v-col>
+          <v-col cols="auto">
+            <v-btn size="x-large" rounded="lg">Cancel</v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+import { ref } from "vue";
+</script>
