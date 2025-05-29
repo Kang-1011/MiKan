f37e064af92ffbcfbbf5390dd995940ddb5f8ca7
Avgnoodl <avgnoodlihateautonicks@gmail.com>
2025-05-29 15:21:57 +0800

    for mytasks page

---
+++ mikan-vuetify/src/pages/TestTaskModal.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-app>
+      <v-main>
+        <v-container class="pa-4">
+          <v-btn color="primary" @click="openDialog = true">
+            Open Task Editor
+          </v-btn>
+          
+          <!-- Task Editor Modal Component -->
+          <TaskEditorModalVuetify v-model="openDialog" />
+        </v-container>
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  import TaskEditorModalVuetify from '../components/VuetifyTaskModal6.vue'
+  
+  const openDialog = ref(false)
+  </script>
+  
+  <style scoped>
+  /* Optional: center content vertically */
+  v-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  </style>
+  
