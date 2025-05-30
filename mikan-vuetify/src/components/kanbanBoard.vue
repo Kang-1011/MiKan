88f170ced5ed4498d69a11b9134da561b598f231
RohimiRaslam <mrohimi@gmail.com>
2025-05-30 09:05:48 +0800

    added files

---
+++ mikan-vuetify/src/components/kanbanBoard.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-card>
+    <kanbanList></kanbanList>
+  </v-card>
+</template>
+
+<script setup>
+import kanbanList from "./kanbanList";
+</script>
---
+++ mikan-vuetify/src/components/kanbanList.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card>
+    <v-btn
+      icon
+      color="success"
+      size="60"
+      class="ma-2 position-fixed bottom-0"
+      @click="dialog = true"
+    >
+      <v-icon size="60">mdi-check</v-icon>
+    </v-btn>
+  </v-card>
+</template>
