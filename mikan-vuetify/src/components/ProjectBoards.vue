42e07651e3207818ea6137a2736d4bec19c2c621
RohimiRaslam <mrohimi@gmail.com>
2025-06-04 14:58:46 +0800

    changed projectboards.vue file from vue 2 to 3

--- mikan-vuetify/src/components/ProjectBoards.vue
+++ mikan-vuetify/src/components/ProjectBoards.vue
@@ -1,21 +1,19 @@
 <template>
-    <div>
-      <KanbanRow
-        v-for="project in projects"
-        :key="project.id"
-        :title="project.title"
-        :cards="project.cards"
-        :headerColor="project.color"
-      />
-    </div>
-  </template>
-  <script>
-  import KanbanRow from './KanbanRow.vue'
-  export default {
-    name: 'ProjectBoards',
-    components: { KanbanRow },
-    props: {
-      projects: Array
-    }
-  }
-  </script>
+  <div>
+    <KanbanRow
+      v-for="project in projects"
+      :key="project.id"
+      :title="project.title"
+      :cards="project.cards"
+      :headerColor="project.color"
+    />
+  </div>
+</template>
+<script setup>
+import { defineProps } from "vue";
+import KanbanRow from "./KanbanRow.vue";
+
+defineProps({
+  projects: Array,
+});
+</script>
