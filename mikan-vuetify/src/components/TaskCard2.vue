cb93b2151b7fecad5cc5c70c8a4645208d9777dd
Avgnoodl <avgnoodlihateautonicks@gmail.com>
2025-06-13 09:13:50 +0800

    Prototype Boards for MyTasks and Kanban Page

---
+++ mikan-vuetify/src/components/TaskCard2.vue
@@ -0,0 +1,23 @@
+<!-- // ========================
+// TaskCard.vue
+// ======================== -->
+<template>
+    <v-card
+      class="mb-2"
+      outlined
+      style="cursor: grab;"
+      @click="$emit('click')"
+    >
+      <v-card-text class="d-flex justify-space-between align-center pa-2">
+        <span>{{ task.title }}</span>
+      </v-card-text>
+    </v-card>
+  </template>
+  
+  <script setup lang="ts">
+  import { defineProps, defineEmits } from 'vue'
+  
+  const props = defineProps({ task: Object, visitorMode: Boolean })
+  
+  defineEmits(['click'])
+  </script>
