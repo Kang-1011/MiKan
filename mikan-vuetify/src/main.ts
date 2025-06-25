b7f5de5db0c6b404547fd9aead05d4ab534e76b1
Avgnoodl <avgnoodlihateautonicks@gmail.com>
2025-06-25 08:58:13 +0800

    umm try this one

--- mikan-vuetify/src/main.ts
+++ mikan-vuetify/src/main.ts
@@ -4,11 +4,8 @@
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
 
-// Plugins
-import { registerPlugins } from "@/plugins";
 
-// Components
-import App from "./App.vue";
+
 
 // Composables
 import { createApp } from "vue";
@@ -16,12 +13,20 @@ import { createPinia } from "pinia";
 
 // Styles
 import "unfonts.css";
+import "vuetify/styles";    // makes sure Vuetify’s CSS variables exist
+import "@/styles/override-font.css"; // <— your override comes last
+
+// Plugins
+import { registerPlugins } from "@/plugins";
+// Components
+import App from "./App.vue";
+import vuetify from './plugins/vuetify'
 
 const app = createApp(App);
-const pinia = createPinia();
 
+app.use(vuetify);
 registerPlugins(app);
-
+const pinia = createPinia();
 app.use(pinia);
 
 app.mount("#app");
