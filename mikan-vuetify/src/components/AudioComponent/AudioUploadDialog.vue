61f56af5754c15860b4dde9e05e60291bb9df0ba
Kang-1011 <148540080+Kang-1011@users.noreply.github.com>
2025-07-24 16:48:32 +0800

    Audio Upload Dialog with Config done

--- mikan-vuetify/src/components/AudioComponent/AudioUploadDialog.vue
+++ mikan-vuetify/src/components/AudioComponent/AudioUploadDialog.vue
@@ -3,45 +3,94 @@
 		<v-card class="rounded-xl pa-2">
 			<v-card-title class="ml-3">Upload Recording</v-card-title>
 
-			<v-card-text>
-				<div v-if="!hasUploaded" class="upload-area d-flex flex-column align-center justify-center"
-					@dragover.prevent="isDragging = true" @dragleave="isDragging = false" @drop.prevent="handleDrop"
-					:class="{ 'drag-over': isDragging }">
-					<v-btn class="mt-5" height="56" @click="triggerFileInput" variant="flat">
-						<v-icon size="50">mdi-file-upload-outline</v-icon>
-					</v-btn>
-					<p>
-						Drop your recording here,
-						<a @click="triggerFileInput" style="cursor: pointer">
-							<u>or click to browse</u>
-						</a>
-					</p>
-				</div>
+			<v-stepper v-model="step" class="no-shadow">
+				<v-stepper-header class="no-shadow">
+					<v-stepper-item :value="1" title="Configuration" />
+					<div class="step-line" />
+					<v-stepper-item :value="2" title="Upload" />
+				</v-stepper-header>
 
-				<div v-else>
-					<UploadFileCustom :files="uploadedFiles" @remove-file="removeFile" />
-				</div>
+				<v-stepper-window>
+					<v-stepper-window-item :value="1">
+						<div>
+							<div style="height: 225px;">
+								<v-form ref="configForm" lazy-validation>
+									<v-text-field v-model="location" label="Location" clearable variant="outlined"
+										density="compact" rounded="lg" class="mb-1 mt-2"
+										:rules="[(v) => !!v || 'Location is required']" />
 
-				<div v-if="!hasUploaded" class="d-flex justify-space-between">
-					<div>
-						<span class="mt-1 text-caption">&#9432;</span>
-						<span class="mt-1 text-caption">
-							Supported files: .mp3, .wav
-						</span>
-					</div>
-					<div>
-						<span class="mt-1 text-caption">
-							Maximum size: 100MB
-						</span>
-					</div>
-				</div>
-			</v-card-text>
+									<v-select v-model="project" :items="projectOptions" item-title="title"
+										item-value="id" label="Project" clearable variant="outlined" density="compact"
+										rounded="lg" class="mb-1" :rules="[(v) => !!v || 'Project is required']" />
 
-			<v-card-actions class="justify-end mb-2">
-				<v-btn color="black" variant="outlined" class="text-body-2 mr-2 rounded-lg" @click="$emit('close-dialog')">Cancel</v-btn>
-				<v-btn color="black" variant="flat" class="text-body-2 mr-4 rounded-lg" @click="transcribeUploadedAudio"
-					:loading="isTranscribing">Transcribe</v-btn>
-			</v-card-actions>
+									<v-select v-model="mode" :items="['Summary', 'Kan']" label="Mode" clearable
+										variant="outlined" density="compact" rounded="lg" class="mb-1"
+										:rules="[(v) => !!v || 'Transcription mode is required']" />
+								</v-form>
+							</div>
+							<div class="d-flex justify-space-between">
+								<v-btn color="black" variant="flat" class="text-body-2 rounded-lg"
+									:disabled="step == 1">
+									Previous
+								</v-btn>
+								<v-btn color="black" variant="flat" class="text-body-2 rounded-lg" @click="goToStep2">
+									Next
+								</v-btn>
+							</div>
+						</div>
+					</v-stepper-window-item>
+
+					<v-stepper-window-item :value="2">
+						<div>
+							<div style="height: 225px;">
+								<div v-if="!hasUploaded"
+									class="upload-area d-flex flex-column align-center justify-center"
+									@dragover.prevent="isDragging = true" @dragleave="isDragging = false"
+									@drop.prevent="handleDrop" :class="{ 'drag-over': isDragging }">
+									<v-btn height="56" @click="triggerFileInput" variant="flat">
+										<v-icon size="50">mdi-file-upload-outline</v-icon>
+									</v-btn>
+									<p>
+										Drop your recording here,
+										<a @click="triggerFileInput" style="cursor: pointer">
+											<u>or click to browse</u>
+										</a>
+									</p>
+								</div>
+
+								<div v-else>
+									<UploadFileCustom :files="uploadedFiles" @remove-file="removeFile" />
+								</div>
+
+								<div v-if="!hasUploaded" class="d-flex justify-space-between">
+									<div>
+										<span class="mt-1 text-caption">&#9432;</span>
+										<span class="mt-1 text-caption">
+											Supported files: .mp3, .wav
+										</span>
+									</div>
+									<div>
+										<span class="mt-1 text-caption">
+											Maximum size: 100MB
+										</span>
+									</div>
+								</div>
+							</div>
+
+							<div class="d-flex justify-space-between">
+								<!-- <v-btn color="black" variant="outlined" class="text-body-2 mr-2 rounded-lg" @click="$emit('close-dialog')">Cancel</v-btn> -->
+								<v-btn color="black" variant="flat" class="text-body-2 rounded-lg" @click="step--">
+									Previous
+								</v-btn>
+								<v-btn color="black" variant="flat" class="text-body-2 rounded-lg"
+									@click="transcribeUploadedAudio" :loading="isTranscribing">
+									Transcribe
+								</v-btn>
+							</div>
+						</div>
+					</v-stepper-window-item>
+				</v-stepper-window>
+			</v-stepper>
 			<input type="file" ref="fileInput" style="display: none" @change="handleFileSelection"
 				accept="audio/mp3,audio/wav" />
 		</v-card>
@@ -49,10 +98,11 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed, onMounted } from "vue";
 import axios from "axios";
 import UploadFileCustom from "../AudioComponent/UploadFileCustom.vue";
 
+import { useProjectStore } from '@/stores/projects'
 import { useTranscriptStore } from "@/stores/transcriptstore";
 const transcriptStore = useTranscriptStore();
 
@@ -65,6 +115,27 @@ const audioFile = ref(null);
 const isTranscribing = ref(false);
 
 const router = useRouter();
+
+const step = ref(1)
+const configForm = ref(null); // instead of using $refs directly
+const location = ref("")
+const project = ref("")
+const mode = ref("")
+
+const projectStore = useProjectStore()
+onMounted(() => {
+	if (projectStore.projects.length === 0) {
+		projectStore.fetchProjects()
+	}
+})
+const projectOptions = computed(() => projectStore.projects)
+
+async function goToStep2() {
+	const { valid } = await configForm.value.validate();
+	if (valid) {
+		step.value = 2;
+	}
+}
 
 defineEmits(["close-dialog"]);
 
@@ -139,5 +210,20 @@ async function transcribeUploadedAudio() {
 	border-radius: 10px;
 	padding: 32px;
 	width: 100%;
+}
+
+.step-line {
+	position: absolute;
+	top: 50%;
+	left: 180px;
+	/* adjust depending on spacing */
+	right: 180px;
+	height: 1px;
+	background-color: grey;
+	z-index: 0;
+}
+
+.no-shadow {
+	box-shadow: none !important;
 }
 </style>
